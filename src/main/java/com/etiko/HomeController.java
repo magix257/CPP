86a5147f7e49a26fb063dbe6a0af6ef2cbc71004
magix257 <55533437+magix257@users.noreply.github.com>
2020-03-02 15:56:15 +0100

    REGEX NA NAZWY PLIKOW ZNALEZIONY

--- src/main/java/com/etiko/HomeController.java
+++ src/main/java/com/etiko/HomeController.java
@@ -29,35 +29,43 @@ public class HomeController {
 		InputStream inStream = null;
 	    OutputStream outStream = null;
 
-		
+		String pattern = ".*\\\\";
 		String inputPath = "F:\\POMOCE NOWE\\PROGRAMOWANIE_JAVA\\SPRING\\CPP\\ARCH\\0\\0001\\0001-300";
 		String outputPath = "F:\\POMOCE NOWE\\PROGRAMOWANIE_JAVA\\SPRING\\CPP\\ROBO\\0001\\0001-300";
 		File[] directoryContent = (new File(inputPath)).listFiles();
-		//List<File> result =  Arrays.asList(directoryContent);
-		new File(outputPath).mkdir();//creates directory
-		File psd = new File(directoryContent[3].toString());
-		File psdc = new File(outputPath+"psdc.ai");
+		String[] filenames = new String[directoryContent.length];
+		String[] abc = new String[directoryContent.length];
+		for (int a = 0; a<directoryContent.length; a++) {
+			filenames[a] = 	directoryContent[a].toString();
+			filenames[a] = filenames[a].replaceAll(pattern, "");
+		}
 		
-		inStream = new FileInputStream(psd);
-        outStream = new FileOutputStream(psdc);
-
-        byte[] buffer = new byte[1024];
-
-        int length;
-		
-        while ((length = inStream.read(buffer)) > 0){  //copy the file content in bytes
-
-            outStream.write(buffer, 0, length);
-
-        }
-
-        inStream.close();
-        outStream.close();
-
+//		new File(outputPath).mkdir();//creates directory
+//		for (int b = 0; b<directoryContent.length; b++) {
+//			
+//		File inp = new File(directoryContent[b].toString());
+//		File out = new File(outputPath+"\\"+abc[b]);
+//		
+//		inStream = new FileInputStream(inp);
+//        outStream = new FileOutputStream(out);
+//
+//        byte[] buffer = new byte[1024];
+//
+//        int length;
+//		
+//        while ((length = inStream.read(buffer)) > 0){  //copy the file content in bytes
+//
+//            outStream.write(buffer, 0, length);
+//
+//        }
+//
+//        inStream.close();
+//        outStream.close();
+//		}
 		
 		//directoryContent[2].createNewFile();
 		
-		m.addAttribute("result", psd);
+		m.addAttribute("result", filenames[3]);
 		
 		return "result.jsp";
 	}
